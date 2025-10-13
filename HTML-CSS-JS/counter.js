c755fbea99bb4167809d9739cafef2f7af5a09d3
SohamDas00 <jisusoham04@gmail.com>
2025-10-14 02:31:45 +0530

    added new things

--- HTML-CSS-JS/counter.js
+++ HTML-CSS-JS/counter.js
@@ -16,3 +16,44 @@ reset.addEventListener('click',()=>{
     countDigit=0;
     count.innerHTML=countDigit;
 })
+
+
+error=document.querySelector(".error");
+shoesButton=document.querySelector(".shoes-button")
+
+shoesButton.addEventListener("click",()=>{
+    error.innerHTML="Something went wrong, please try again."
+    error.style.color="red";
+})
+
+
+let co1=0;
+let co2=0;
+let cou1=document.querySelector(".count1")
+let cou2=document.querySelector(".count2")
+let one=document.querySelector('#one')
+let two=document.querySelector("#two")
+let three=document.querySelector('#three')
+let four=document.querySelector('#four')
+let five=document.querySelector("#five")
+let six=document.querySelector('#six')
+
+one.addEventListener('click',()=>{
+    cou1.innerHTML=co1+=1;
+})
+two.addEventListener('click',()=>{
+    cou1.innerHTML=co1+=2;
+})
+three.addEventListener('click',()=>{
+    cou1.innerHTML=co1+=3;
+})
+
+four.addEventListener('click',()=>{
+    cou2.innerHTML=co1+=1;
+})
+five.addEventListener('click',()=>{
+    cou2.innerHTML=co1+=2;
+})
+six.addEventListener('click',()=>{
+    cou2.innerHTML=co1+=3;
+})
