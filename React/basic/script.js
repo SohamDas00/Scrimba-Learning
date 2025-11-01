08e93c40e99f36a3a23a66610a4fe25a1d7ac952
SohamDas00 <jisusoham04@gmail.com>
2025-11-01 20:16:59 +0530

    added script.js file

---
+++ React/basic/script.js
@@ -0,0 +1,11 @@
+const heading=document.querySelector(".heading")
+// heading.innerHTML="Hello from js"
+
+import name from "./test.js"
+import { age } from "./test.js";
+console.log(name,age);
+heading.innerHTML=name+" "+age
+
+//import and export
+//real DOM and Virtual DOM
+ 
