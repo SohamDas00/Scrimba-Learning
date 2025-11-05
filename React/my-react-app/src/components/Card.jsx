af0921d50aabfc0e6f7c801e6d8bbcec0ee536e2
SohamDas00 <jisusoham04@gmail.com>
2025-11-05 22:30:52 +0530

    added basic example file

---
+++ React/my-react-app/src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h2 className="heading">Soham Das</h2>
+        <p className="heading">3rd Yr CSE || MAKAUT || 25500123144</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
